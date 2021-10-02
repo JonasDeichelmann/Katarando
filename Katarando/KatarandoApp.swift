ed5f7a487e778cfab8fc36b245209d216ec58592
JonasDeichelmann <jonas.deichelmann@me.com>
2021-10-02 15:45:41 +0200

    Add project

---
+++ Katarando/KatarandoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KatarandoApp.swift
+//  Katarando
+//
+//  Created by Jonas Deichelmann on 02.10.21.
+//
+
+import SwiftUI
+
+@main
+struct KatarandoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
